2941c00ae7e45b556d5266a177f2155247860395
Alex Wood <alexwood319@gmail.com>
2022-10-28 12:34:05 -0700

    Added binary file streams

--- FileStreamProject/main.cpp
+++ FileStreamProject/main.cpp
@@ -4,6 +4,10 @@
 
 int main()
 {
+	//
+	// TEXT FILES
+	//
+
 	// Writing to a File
 	std::fstream bioFile;
 	bioFile.open("bio.txt", std::ios::out);
@@ -33,6 +37,23 @@ int main()
 		std::cout << temp << std::endl;
 	}
 	readFile.close();
+
+	//
+	// BINARY FILES
+	//
+	
+	std::fstream saveFile;
+	saveFile.open("save.dat", std::ios::out | std::ios::binary);
+	int score = 32;
+	saveFile.write((const char*)&score, sizeof(int));
+	saveFile.close();
+
+	std::fstream loadFile;
+	loadFile.open("save.dat", std::ios::in | std::ios::binary);
+	int lastScore = 0;
+	loadFile.read((char*)&lastScore, sizeof(int));
+	loadFile.close();
+	std::cout << "Last Score: " << lastScore << std::endl;
 
 	return 0;
 }
